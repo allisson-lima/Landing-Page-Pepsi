8761aced40443fa8e6217ea0cf468e4988b65409
Allisson-Lima-Dev <allysoncosta@outlook.com>
2021-06-10 16:05:36 -0300

    end

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 function imgslider(anything){
     document.querySelector('.pepsi').src = anything;
 }
+function changeBgcolor(color){
+    const sec = document.querySelector('.sec');
+    sec.style.background = color;
+}
